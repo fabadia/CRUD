50a99f5e94587d4c7d494cb04cda190aa088f199
fabadia <fabadia@bol.com.br>
2017-05-23 12:44:45 -0300

    Adicionando Navegação e Home.

--- CRUD/src/app.ts
+++ CRUD/src/app.ts
@@ -1,3 +1,10 @@
 export class App {
-  message = 'Hello World!';
+    private router;
+    configureRouter(config, router) {
+        config.title = 'Aurelia CRUD Demo';
+        config.map([
+            { route: ['', 'home'], name: 'home', moduleId: 'home', nav: true, title: 'Home' }
+        ]);
+        this.router = router;
+    }
 }

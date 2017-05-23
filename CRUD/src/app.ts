dc75410af5be19d7a4e0e9466cc1546159816750
fabadia <fabadia@bol.com.br>
2017-05-23 19:55:52 -0300

    Adicionando lista de Candidatos.

--- CRUD/src/app.ts
+++ CRUD/src/app.ts
@@ -3,7 +3,8 @@ export class App {
     configureRouter(config, router) {
         config.title = 'Aurelia CRUD Demo';
         config.map([
-            { route: ['', 'home'], name: 'home', moduleId: 'home', nav: true, title: 'Home' }
+            { route: ['', 'home'], name: 'home', moduleId: 'home', nav: true, title: 'Home' },
+            { route: 'candidatos', name: 'candidatos', moduleId: 'candidatos', nav: true, title: 'Candidatos' }
         ]);
         this.router = router;
     }
---
+++ CRUD/src/candidatos.ts
@@ -0,0 +1,21 @@
+﻿import { HttpClient } from 'aurelia-fetch-client';
+import { autoinject } from 'aurelia-framework';
+
+@autoinject
+export class Candidatos {
+    public candidatos = [];
+
+    constructor(private http: HttpClient) {
+        http.configure(config => {
+            config
+                .useStandardConfiguration()
+                .withBaseUrl('/api/Candidatos');
+        });
+    }
+
+    activate() {
+        return this.http.fetch("")
+            .then(response => response.json())
+            .then(candidatos => this.candidatos = <any>candidatos);
+    }
+}
